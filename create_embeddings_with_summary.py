9559ec3bc320bed020442de6d5f1d4016d40f63b
ratniel15 <ratniel.cdac@gmail.com>
2024-02-20 00:54:00 +0530

    embeddings on summary

--- create_embeddings_with_summary.py
+++ create_embeddings_with_summary.py
@@ -1,13 +1,10 @@
 from rag.embed_nodes import embed_nodes,embed_nodes_on_summary
+from rag.rag_utils import load_docs
 
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.node_parser import SentenceSplitter
 
-sentence_splitter = SentenceSplitter(chunk_size=1024, chunk_overlap=200)
-dir_path = "/home/dai/35/rag/data/text_data/Articles"
-reader = SimpleDirectoryReader(dir_path, recursive=True)
-docs = reader.load_data(show_progress=True)
-nodes = sentence_splitter.get_nodes_from_documents(docs, show_progress=True)
+nodes = load_docs("/home/dai/rag/Articles_store/docstore/Articles_with_summary_revised")
 
 
 nodes = nodes = embed_nodes_on_summary(nodes=nodes, save_dir="/home/dai/35/rag/Articles_store/docstore", docstore_name="Articles_with_embeddings_with_summary", save_docstore=True)
